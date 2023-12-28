528a113e9fd331fee4422229ed625498c3b51e01
호제 <65165767+kuku-forum@users.noreply.github.com>
2023-12-28 20:40:46 +0900

    Title: palindrome 여부 판단하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool check(string *tmp){
+
+
+    int len = (*tmp).length() - 1;
+    
+    for (int i = 0; i <= len; i++){
+        if( (*tmp)[i] != (*tmp)[len -i]){
+            return false;
+        }
+    }
+    return true;
+
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    if (check(&str))
+        cout << "Yes";
+    else
+        cout << "No"; 
+
+    return 0;
+}
