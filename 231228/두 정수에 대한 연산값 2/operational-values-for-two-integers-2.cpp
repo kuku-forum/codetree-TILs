d296580556b74a03e8975c47f00dbcaa56ccf92f
호제 <65165767+kuku-forum@users.noreply.github.com>
2023-12-28 22:38:01 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void func(int &a, int &b){
+    if (a > b){
+        b += 10;
+        a *= 2;
+    }
+    else{
+        a += 10;
+        b *= 2;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    func(a, b);
+
+    cout << a << ' ' << b;
+
+    return 0;
+}
