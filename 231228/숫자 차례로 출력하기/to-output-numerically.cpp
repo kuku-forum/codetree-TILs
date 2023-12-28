020409a62bf43f0bceae3448eb8bc364df408304
호제 <65165767+kuku-forum@users.noreply.github.com>
2023-12-28 23:39:36 +0900

    Title: 숫자 차례로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231228/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 231228/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -2,30 +2,28 @@
 
 using namespace std;
 
-int num = 1;
-
 void upStream(int n){
     if (n == 0)
         return;
 
-    cout << num++ << ' ';
-    upStream(--n);
+    upStream(n-1);
+    cout << n << ' ';
 }
 
 void downStream(int n){
     if (n == 0)
         return;
 
-    cout << num-- << ' ';
-    downStream(--n);
+    cout << n << ' ';
+    downStream(n-1);
 }
 int main() {
     // 여기에 코드를 작성해주세요.
     int N;
     cin >> N;
+
     upStream(N);
     cout << endl;
-    num--;
     downStream(N);
 
     return 0;
