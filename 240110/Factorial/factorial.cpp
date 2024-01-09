ab2fca289a67bc354855166b6042a02f5eefb30e
호제 <65165767+kuku-forum@users.noreply.github.com>
2024-01-10 00:50:48 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/Factorial/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int n){
+    if(n == 0)
+        return 1;
+
+    return n * func(n-1);
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    cout << func(N);
+
+    return 0;
+}
