83b652effd3cd1178185d09b47fbdb2d3a1e04a0
호제 <65165767+kuku-forum@users.noreply.github.com>
2024-01-11 01:58:56 +0900

    Title: 중앙값 계산 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240111/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int N, arr[10] ={};
+    cin >> N;
+    int tmp;
+
+    for(int i = 0; N> i; i++){
+        cin >> tmp;
+        arr[i] = tmp;
+
+        if(i%2 == 0){
+            sort(arr, arr+i+1);
+            cout << arr[i/2] << ' ';
+        }
+
+    }
+
+    return 0;
+}
