1aeed7a5f8cf7c60da036308d25b634ad374ec00
호제 <65165767+kuku-forum@users.noreply.github.com>
2024-01-11 00:22:15 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/문자열 정렬/string-sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    string str;
+    cin >> str;
+
+    sort(str.begin(), str.end());
+
+    cout << str;
+    return 0;
+}
