08d26e6b78966d1917cdbb984aad4f895160257a
호제 <65165767+kuku-forum@users.noreply.github.com>
2024-01-16 02:15:22 +0900

    Title: 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/줄 세우기/line-up-students.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Info{
+    public:
+        int w, h, i;
+
+        Info(int w, int h, int i){
+            this->w = w;
+            this->h = h;
+            this->i = i;
+        }
+        Info(){}
+} info[1001];
+
+bool cmp(const Info &a, const Info &b){
+    if(a.h != b.h)
+        return a.h > b.h;
+    
+    if(a.w != b.w)
+        return a.w > b.w;
+
+    return a.i < b.i;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=0; n>i; i++){
+        cin >> info[i].w >> info[i].h;
+        info[i].i = i+1;
+    }
+
+    sort(info, info+n, cmp);
+
+    for (int i=0; n>i; i++)
+        cout << info[i].w << ' ' << info[i].h << ' ' << info[i].i << endl;
+
+    return 0;
+}
