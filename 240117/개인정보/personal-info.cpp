d1acdafd691209aa54bf61d4084b4125371ad61e
호제 <65165767+kuku-forum@users.noreply.github.com>
2024-01-17 00:05:39 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240117/개인정보/personal-info.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Info{
+    public:
+        string name;
+        int h;
+        float w;
+
+        Info(string name, int h, float w){
+            this->name = name;
+            this->h = h;
+            this->w = w;
+        }
+        Info(){}
+} info[5];
+
+bool cmpN(const Info &a, const Info &b){
+    return a.name < b.name;
+}
+
+bool cmpH(const Info &a, const Info &b){
+    return a.h > b.h;
+}
+
+int main() {
+    
+    for(int i=0; 5>i; i++){
+        cin >> info[i].name >> info[i].h >> info[i].w;
+    }
+
+    sort(info, info+5, cmpN);
+    cout << "name" << endl;
+
+    for(int i=0; 5>i; i++)
+        cout << info[i].name << ' ' << info[i].h << ' ' << info[i].w << ' ' << endl;
+    
+    sort(info, info+5, cmpH);
+    cout << endl << "height" << endl;
+
+    for(int i=0; 5>i; i++)
+        cout << info[i].name << ' ' << info[i].h << ' ' << info[i].w << ' ' << endl;
+
+    return 0;
+}
