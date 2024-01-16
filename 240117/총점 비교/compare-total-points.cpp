f2af0b5f6eef89ac0aa065aa376f516221bb8e39
호제 <65165767+kuku-forum@users.noreply.github.com>
2024-01-17 00:53:20 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/총점 비교/compare-total-points.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Info{
+    public:
+        int a, b, c;
+        string name;
+
+        Info(string name, int a, int b, int c){
+            this->name = name;
+            this->a = a;
+            this->b = b;
+            this->c = c;
+        }
+        Info(){}
+} info[10];
+
+bool cmp(const Info &a, const Info &b){
+    int sumA = a.a + a.b + a.c;
+    int sumB = b.a + b.b + b.c;
+    return sumA < sumB;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=0 ; n > i; i++)
+        cin >> info[i].name >> info[i].a >> info[i].b >> info[i].c;
+
+    sort(info, info+n, cmp);
+
+    for (int i=0 ; n > i; i++)
+        cout << info[i].name << ' ' << info[i].a << ' ' <<  info[i].b << ' ' <<  info[i].c << endl;
+
+    return 0;
+}
